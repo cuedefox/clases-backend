2f4ef9c69d4de8c7e3860c4e6a3695dcecfabf83
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2022-11-19 00:34:38 -0300

    termino clase 10

---
+++ clase-10/ejercicio_2/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/datos', (req, res) => {
+    res.render('pages/nivel', req.query);
+})
+
+app.listen(8080);
